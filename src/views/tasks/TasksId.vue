6f8802ceba32bd60bad33f68d70fbe95563842c0
Suleiman <93479133+henexy@users.noreply.github.com>
2023-02-25 15:59:12 +0300

    fiver

--- src/views/tasks/TasksId.vue
+++ src/views/tasks/TasksId.vue
@@ -5,30 +5,28 @@
             <h2 class="header__title ml-5 text-2xl">{{ getTask.name }}</h2>
         </div>
         <div class="textarea mt-6 flex gap-6">
-            <textarea class="textarea__input h-542 bg-light-grey p-5 outline-none resize-none w-full"
-                placeholder="Кодинг"></textarea>
-            <div class="textarea__result bg-light-grey p-5 w-647">Тест</div>
+            <!-- <textarea class="textarea__input h-542 bg-light-grey p-5 outline-none resize-none w-full"
+                placeholder="Кодинг"></textarea> -->
+            <CodeEditor width="100%" height="542px" border_radius="12px" v-model="code" />
+            <div class="textarea__result bg-light-grey p-5 w-647 rounded-lg">Тест</div>
         </div>
         <div class="description mt-9">
             <h2 class="description__title text-3xl">Задача</h2>
             <p class="description__subtitle text-lg max-w-3xl mt-4">Мы вынуждены отталкиваться от того, что
-                высококачественный прототип будущего
-                проекта в значительной степени обусловливает важность кластеризации усилий. Картельные сговоры не допускают
-                ситуации, при которой акционеры крупнейших компаний набирают популярность среди определенных слоев населения
+                {{getTask.description}}
             </p>
         </div>
         <div class="actions flex justify-between mt-20">
             <div class="actions__left flex gap-3">
-                <base-button size="md" rounded="lg" theme="success">Завершить задачу</base-button>
+                <base-button size="md" rounded="lg" theme="success">Проверить</base-button>
                 <base-button @click="isShowModal = true" size="md" rounded="lg" theme="primary">Посмотреть
                     решение</base-button>
             </div>
             <base-button size="md" rounded="lg" theme="primary">Сохранить и выйти</base-button>
         </div>
         <transition name="slide-fade">
-            <base-modal v-show="isShowModal === true" class="modal absolute" @closeModal="isShowModal = $event"
+            <base-modal v-show="isShowModal === true" class="modal z-10 absolute" @closeModal="isShowModal = $event"
                 @success="isShowModal = $event">
-
             </base-modal>
         </transition>
     </div>
@@ -36,12 +34,14 @@
 
 <script>
 import { mapState } from 'vuex';
+import CodeEditor from 'simple-code-editor';
 import arrowLeft from '@/assets/img/arrow-left.svg'
 import BaseModal from '@/components/Home/BaseModal.vue'
 import BaseButton from '@/components/UI/BaseButton.vue'
 
 export default {
     components: {
+        CodeEditor,
         arrowLeft,
         BaseModal,
         BaseButton,
@@ -49,7 +49,11 @@ export default {
     data() {
         return {
             isShowModal: false,
+            code: ''
         }
+    },
+    mounted () {
+        this.code = `// ${this.getTask.description}`
     },
     computed: {
         ...mapState(['tasks']),
