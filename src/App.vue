eaa52764bc6195e7138bab10b4089483d1baf99f
Danila <danila.barkov@gmail.com>
2023-02-27 18:59:10 +0300

    add animations pages\task list

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app" class="bg-main">
-    <router-view />
+    <transition name="slide-fade" mode="out-in">
+      <router-view />
+    </transition>
     <modals-controller />
   </div>
 </template>
@@ -15,5 +17,15 @@ export default {
 </script>
 
 <style lang="scss">
-
+.slide-fade-enter-active {
+  transition: all .8s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active до версии 2.1.8 */ {
+  transform: translateX(100%);
+  opacity: 1;
+}
 </style>
--- src/components/Home/TasksList.vue
+++ src/components/Home/TasksList.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="tasks-list mt-11 flex flex-col gap-6">
-        <tasks-card class="tasks-list__item" v-for="task in tasksList" :key="task.id" :task="task" />
+    <div class="tasks-list mt-11">
+        <transition-group name="slide-fade" class="flex flex-col gap-6" mode="out-in">
+            <tasks-card class="tasks-list__item" v-for="task in tasksList" :key="task.id" :task="task" />
+        </transition-group>
     </div>
 </template>
 
@@ -21,5 +23,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.slide-fade-enter-active {
+    transition: all .3s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+  /* .slide-fade-leave-active до версии 2.1.8 */ {
+    transform: translateX(50px);
+    opacity: 0;
+  }
 </style>
