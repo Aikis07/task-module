92b55b10240c18f3ff9883695d436a519c76f67c
Danila <danila.barkov@gmail.com>
2023-03-10 21:13:04 +0300

    fix code

--- src/App.vue
+++ src/App.vue
@@ -9,29 +9,13 @@
 
 <script>
 import ModalsController from "@/components/Modals/ModalsController.vue"
-import { mapMutations } from "vuex";
 
 export default {
   components: {
     ModalsController,
   },
-  data() {
-    return {
-
-    }
-  },
-  methods: {
-    ...mapMutations({ setModal: 'modals/setModal' }),
-    // hasUser(statusCode) {
-    //   if (statusCode !== 200) {
-    //     this.setModal({ name: "RedirectModal", data: 'Вы будете перенаправлены на главную страницу через 5 секунд' })
-    //   }
-    // },
-  },
-  async created() {
+  created() {
     document.cookie = 'user=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbmV4eUBtYWlsLnJ1IiwiaWQiOjEsImlhdCI6MTY3ODEyMzkyMywiZXhwIjoxNjc4MjEwMzIzfQ.aAQqD5McoPHERIwkaTSMiUmEhG7VgnjdL8C12uKmqjQ'
-    // const statusCode = await this.$store.dispatch('getUserToken')
-    // this.hasUser(statusCode.status)
   },
 };
 </script>
